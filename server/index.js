15f9cc3ab8a615200cd88d4a600eb1166da273ea
김수빈 <rlatnqls311@gmail.com>
2024-10-24 21:52:00 +0900

    타임스탬프 가독성 수정

--- server/index.js
+++ server/index.js
@@ -21,13 +21,29 @@ const io = new Server(httpServer, {
 
 let messages = [];
 
+let today = new Date();
+
+let todayFormat =
+  today.getFullYear() +
+  "년 " +
+  (today.getMonth() + 1) +
+  "월 " +
+  today.getDate() +
+  "일 " +
+  today.getHours() +
+  "시 " +
+  today.getMinutes() +
+  "분 " +
+  today.getSeconds() +
+  "초";
+
 io.on("connection", (socket) => {
-  socket.broadcast.emit("SEND_MESSAGE", { // 새로운 사용자 입장 메세지
+  io.emit("SEND_MESSAGE", { // 새로운 사용자 입장 메세지
     id: `${socket.id} - ${Math.random()}`, //"-"는 그냥 구분하기위한 선
     content: `${socket.id}이 입장했습니다.`,
     sender: socket.id,
-    timestamp: new Date(),
-  }); 
+    timestamp: todayFormat,
+  });
   console.log("사용자가 연결되었습니다", socket.id);
 
   // 메세지 송수신 구현, 메시지 객체는 id, content, timestamp 필드를 포함해야 합니다
@@ -36,10 +52,10 @@ io.on("connection", (socket) => {
     // const parseMessage = JSON.parse(msg); // 문자열을 객체로 변환
 
     const message = {
-      id: `${socket.id}-${Math.random()}`,
+      id: `${socket.id} - ${Math.random()}`, //"-"는 그냥 구분하기위한 선
       content: msg,
       sender: socket.id,
-      timestamp: new Date(),
+      timestamp: todayFormat,
     };
 
     // 메시지를 전송할 때마다 서버에 메시지를 저장하세요.
@@ -51,11 +67,6 @@ io.on("connection", (socket) => {
   socket.on("disconnect", () => {
     console.log("사용자가 연결을 끊었습니다", socket.id);
   });
-});
-
-// GET /messages 엔드포인트를 추가하여, 서버에 저장된 모든 채팅 기록을 반환하세요
-app.get("/messages", (req, res) => {
-  res.json(messages);
 });
 
 // GET /messages 엔드포인트를 추가하여, 서버에 저장된 모든 채팅 기록을 반환하세요
